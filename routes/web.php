0a1973b27b2d37226f022eb45269d0ccf19722dc
skysee6126 <chooshinhye@gmail.com>
2021-07-17 23:14:37 +0900

    controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,24 +19,5 @@ Route::get('/', function () {
 });
 
 
-Route::get('hello/{msg}', function($msg) {
-    $html = <<<EOF
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body { font-size: 16pt; color: #999; }
-    h1 { font-size:100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px; }
-    </style>
-    </head>
-    <body>
-        <h1>Hello</h1>
-        <p>{$msg}</p>
-        <p>This is sample page.</p>
-    </body>
-    </html>
-    EOF;
-
-        return $html;
-        
-});
+Route::get('hello', 'HelloController@index');
+Route::get('hello/other', 'HelloController@other');
