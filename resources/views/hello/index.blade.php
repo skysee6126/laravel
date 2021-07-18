6b7e392aeab3d10f5eb597d027e4f785ef7bf99f
skysee6126 <chooshinhye@gmail.com>
2021-07-18 15:54:52 +0900

    include

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,24 +1,20 @@
-<html>
-<head>
-  <title>Hello/Index</title>
-  <style>
-    body { font-size: 16pt; color: #999; }
-    h1 { font-size:100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px; }
-  </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;for derective</p>
-    <ol>
-    @foreach($data as $item)
-    @if ($loop->first)
-    <p>*Data list</p><ul>
-    @endif
-      <li>No, {{ $loop->iteration }}.{{ $item }}</li>
-    @if ($loop->last)
-  </ul><p>Until this line</p>
-    @endif
-    @endforeach
-  </ol>
-</body>
-</html>
+@extends('layouts.helloapp')
+@section('title', 'Index')
+
+@section('menubar')
+  @parent
+  index page
+@endsection
+
+@section('content')
+  <p>This is content</p>
+
+  @include('components.message', ['msg_title'=>'OK',
+    'msg_content'=>'This is sub-view'])
+
+    @endsection
+
+@section('footer')
+  copyright 2020 tuyano.
+@endsection
+
