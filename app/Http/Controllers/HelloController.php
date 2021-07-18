981613511d81b02fb4528cc0225ee0e239e6fd59
skysee6126 <chooshinhye@gmail.com>
2021-07-18 18:40:40 +0900

    Service provider

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,11 +9,6 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index() {
-        $data = [
-            ['name'=> 'Yamada', 'email'=> 'yamada@taro'],
-            ['name'=> 'hanako', 'email'=> 'hanako@flower'],
-            ['name'=> 'sachiko', 'email'=> 'sachiko@happy']
-        ];
-        return view('hello.index', ['data'=>$data]);
+        return view('hello.index', ['message'=>'Hello!']);
     }
 }
