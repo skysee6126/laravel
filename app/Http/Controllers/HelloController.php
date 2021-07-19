7c73c8256c145eea89f5741b26af3721030658e9
skysee6126 <chooshinhye@gmail.com>
2021-07-19 23:10:25 +0900

    custormised validator

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,47 +11,11 @@ class HelloController extends Controller
 {
     public function index(Request $request) {
 
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if ($validator->fails()) {
-            $msg = 'There is some issue in a query';
-        } else {
-            $msg = 'ID/PASS are received. Please fill out the form';
-        }
-        return view('hello.index', ['msg'=>$msg, ]);
+        return view('hello.index', ['msg'=>'please fill out the form' ]);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric',
-        ];
-
-        $messages = [
-            'name.required' => '이름을 반드시 입력해주세요',
-            'mail.email' => '메일주소를 반드시 입력해주세요',
-            'age.numeric' => '연령은 정수를 입력해주세요',
-            'age.min' => '연령은 0이상의 숫자를 입력해주세요',
-            'age.max' => '연령은 150 이하의 숫자를 입력해주세요',
-        ];
-
-        $validator = Validator::make($request->all(), $rules, $messages);
-        $validator->sometimes('age', 'min:0', function($input){
-            return is_numeric($input->age);
-        });
-        $validator->sometimes('age', 'max:150', function($input){
-            return is_numeric($input->age);
-        });
-        
-        if ($validator->fails()){
-            return redirect('/hello')
-                ->withErrors($validator)
-                ->withInput();
-        }
         return view('hello.index', ['msg'=>'there is no issue!']);
     }
 
--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -30,7 +30,7 @@ class HelloRequest extends FormRequest
         return [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|between:0, 150',
+            'age' => 'numeric|hello',
         ];
     }
     
@@ -40,7 +40,7 @@ class HelloRequest extends FormRequest
             'name.required' => '이름을 반드시 입력해주세요',
             'mail.email' => '메일주소를 반드시 입력해주세요',
             'age.numeric' => '연령은 정수를 입력해주세요',
-            'age.between' => '연령은 0~150 사이의 숫자를 입력해주세요',
+            'age.hello' => 'hello! 입력은 홀수만 입력 가능합니다',
         ];
     }
 
