60732e2cf7b3ae724d0b000a09d7af8d1d0fc107
skysee6126 <chooshinhye@gmail.com>
2021-07-21 17:56:18 +0900

    crud

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -15,18 +15,54 @@ class HelloController extends Controller
         $items = DB::select('select * from people');
         return view('hello.index', ['items'=>$items]);
     }
-
-    public function post(Request $request)
-    {
-        $validate_rule = [
-            'msg' => 'required',
-        ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = response()->view('hello.index', 
-            ['msg'=> '['.$msg.'] is saved on Cookie.']);
-        $response->cookie('msg', $msg, 100);
-        return $response;
+    
+    public function post(Request $request) {
+        
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items'=>$items]);
     }
 
+    public function add(Request $request) {
+        return view('hello.add');
+    }
+
+    public function create(Request $request) {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
+    }
+    
+    public function edit(Request $request) {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello/edit', ['form' => $item[0]]);
+    }
+    
+    public function update(Request $request) {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::update('update people set name =:name, mail =:mail, age=:age where id =:id', $param);
+        return redirect('/hello');
+    }
+
+    public function del(Request $request) {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello/del', ['form' => $item[0]]);
+    }
+    
+    public function remove(Request $request) {
+        $param = ['id' => $request->id];
+        DB::delete('delete from people where id =:id', $param);
+        return redirect('/hello');
+    }
 }
