565f6c03660b6017dc0da1efdc75d7cc57fa9bf4
skysee6126 <chooshinhye@gmail.com>
2021-07-22 11:07:36 +0900

    seed

---
+++ database/seeders/PeopleTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => 'taro',
+            'mail' => 'taro@taro.jp',
+            'age' => 12,
+        ];
+        DB::table('people')->insert($param);
+
+        $param = [
+            'name' => 'hana',
+            'hana' => 'hana@taro.jp',
+            'age' => 32,
+        ];
+        DB::table('people')->insert($param);
+
+        $param = [
+            'name' => 'mako',
+            'mako' => 'mako@taro.jp',
+            'age' => 24,
+        ];
+        DB::table('people')->insert($param);
+    }
+}
