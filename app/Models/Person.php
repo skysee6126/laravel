ce51caee0e6dc8045c104e22acd0df2383b3918e
skysee6126 <chooshinhye@gmail.com>
2021-07-24 17:22:43 +0900

    local scope

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -12,4 +12,16 @@ class Person extends Model
     public function getData() {
         return $this->id.':'.$this->name.'('.$this->age.')';
     }
+
+    public function scopeNameEqual($query, $str) {
+        return $query->where('name', $str);
+    }
+    
+    public function scopeAgeGreaterThan($query, $n) {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n) {
+        return $query->where('age', '<=', $n);
+    }
 }
