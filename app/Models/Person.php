ac8e098a9631620f54dc593a28c33723e1001ce2
skysee6126 <chooshinhye@gmail.com>
2021-07-24 17:40:31 +0900

    global scope

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
     use HasFactory;
+
+    protected static function boot() {
+        parent::boot();
+        static::addGlobalScope(new ScopePerson);
+    }
 
     public function getData() {
         return $this->id.':'.$this->name.'('.$this->age.')';
