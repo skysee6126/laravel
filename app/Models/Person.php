06509a7c3bcac2b84e1c342ac02bb3633a6f7ecb
skysee6126 <chooshinhye@gmail.com>
2021-07-24 20:20:52 +0900

    model_crud

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -11,24 +11,15 @@ class Person extends Model
 {
     use HasFactory;
 
-    protected static function boot() {
-        parent::boot();
-        static::addGlobalScope(new ScopePerson);
-    }
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
 
     public function getData() {
         return $this->id.':'.$this->name.'('.$this->age.')';
-    }
-
-    public function scopeNameEqual($query, $str) {
-        return $query->where('name', $str);
-    }
-    
-    public function scopeAgeGreaterThan($query, $n) {
-        return $query->where('age', '>=', $n);
-    }
-
-    public function scopeAgeLessThan($query, $n) {
-        return $query->where('age', '<=', $n);
     }
 }
